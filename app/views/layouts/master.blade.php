34834c3cb81fcd87a0b7d0464b7b194ca2ab7493
Lukas Feistritzer <luki.feise@gmail.com>
2016-02-25 13:59:31 +0100

    .

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -140,13 +140,12 @@
 		</div>
 		
 
-		<script src="http://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"/>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     	
-		<script type="text/javascript">
-
-		</script>
+		
 
     	@yield('scripts')
     </body>
