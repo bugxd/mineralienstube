62ca411eaa929741420fd2d6e305184a4a02aa76
Lukas Feistritzer <luki.feise@gmail.com>
2016-02-25 18:12:36 +0100

    .

--- app/views/pages/findYstone.blade.php
+++ app/views/pages/findYstone.blade.php
@@ -31,27 +31,27 @@ img {
 	<img class="img-responsive" src="/images/findYstone/aura.jpg" max-width="500" max-hight="500" alt = "Maennchen" usemap="#Maennchen">
 	
 		<map name="Maennchen">					
-			<area id="Kronenchakra" shape="circle" coords="175,103, 12" href="{{ URL::route('stones-search-param', 'Kronen- oder Scheitelchakra') }}"
-			alt="Kronenchakra" title="Kronenchakra">
-			<area id="Stirnchakra" shape="circle" coords="214,130,12" href="{{ URL::route('stones-search-param', 'Stirnchakra oder Drittes Auge') }}"
-			alt="Stirnchakra" title="Stirnchakra">
-			<area id="Halschakra" shape="circle" coords="112,172,16" href="{{ URL::route('stones-search-param', 'Hals- oder Kehlchakra') }}"
+			<area id="Kronenchakra" shape="circle" coords="159,31, 15" href="{{ URL::route('stones-search-param', 'Kronen- oder Scheitelchakra') }}"
+			alt="Kronen- oder Scheitelchakra" title="Kronenchakra">
+			<area id="Stirnchakra" shape="circle" coords="208,65,15" href="{{ URL::route('stones-search-param', 'Stirnchakra oder Drittes Auge') }}"
+			alt="Stirnchakra oder Drittes Auge" title="Stirnchakra">
+			<area id="Halschakra" shape="circle" coords="78,118,19" href="{{ URL::route('stones-search-param', 'Hals- oder Kehlchakra') }}"
 			alt="Halschakra" title="Halschakra">
-			<area id="Herzchakra" shape="circle" coords="267,213,14" href="{{ URL::route('stones-search-param', 'Herzchakra') }}"
+			<area id="Herzchakra" shape="circle" coords="274,170,17" href="{{ URL::route('stones-search-param', 'Herzchakra') }}"
 			alt="Herzchakra" title="Herzchakra">
-			<area id="Solarplexuschakra" shape="circle" coords="257,266,12" href="{{ URL::route('stones-search-param', 'Nabel- oder Solarplexuschakra') }}"
+			<area id="Solarplexuschakra" shape="circle" coords="262,236,15" href="{{ URL::route('stones-search-param', 'Nabel- oder Solarplexuschakra') }}"
 			alt="Solarplexuschakra" title="Solarplexuschakra">
-			<area id="Sakralchakra" shape="circle" coords="88,306,13" href="{{ URL::route('stones-search-param', 'Sakral- oder Sexualchakra') }}"
+			<area id="Sakralchakra" shape="circle" coords="50,286,16" href="{{ URL::route('stones-search-param', 'Sakral- oder Sexualchakra') }}"
 			alt="Sakralchakra" title="Sakralchakra">
-			<area id="Wurzelchakra" shape="circle" coords="251,322,13" href="{{ URL::route('stones-search-param', 'Wurzel- oder Basischakra') }}"
+			<area id="Wurzelchakra" shape="circle" coords="255,306,16" href="{{ URL::route('stones-search-param', 'Wurzel- oder Basischakra') }}"
 			alt="Wurzelchakra" title="Wurzelchakra">
 			
-			<area id="Kopf" shape="rect" coords="156,132,192,151" href="{{ URL::route('stones-search-param', 'Kopf') }}" alt="Kopf">
-			<area id="Lunge" shape="rect" coords="150,203,194,220" href="{{ URL::route('stones-search-param', 'Lunge') }}" alt="Lunge">
-			<area id="Magen" shape="rect" coords="147,246,196,263" href="{{ URL::route('stones-search-param', 'Magen') }}" alt="Magen">
-			<area id="Bauch" shape="rect" coords="149,287,191,300" href="{{ URL::route('stones-search-param', 'Bauch') }}" alt="Bauch">
-			<area id="Gelenke" shape="rect" coords="144,381,200,395," href="{{ URL::route('stones-search-param', 'Gelenke') }}" alt="Gelenke">
-			<area id="Beine" shape="rect" coords="155,425,195,439" href="{{ URL::route('stones-search-param', 'Beine') }}" alt="Beine">
+			<area id="Kopf" shape="rect" coords="136,69,185,91" href="{{ URL::route('stones-search-param', 'Kopf') }}" alt="Kopf">
+			<area id="Hals" shape="rect" coords="173,112,215,132" href="{{ URL::route('stones-search-param', 'Hals') }}" alt="Hals">
+			<area id="Gelenke" shape="rect" coords="39,154,114,175" href="{{ URL::route('stones-search-param', 'Gelenke') }}" alt="Gelenke">
+			<area id="Brust" shape="rect" coords="133,172,188,191" href="{{ URL::route('stones-search-param', 'Brust') }}" alt="Brust">
+			<area id="Bauch" shape="rect" coords="133,241,190,261," href="{{ URL::route('stones-search-param', 'Bauch') }}" alt="Bauch">
+			<area id="Beine" shape="rect" coords="134,344,185,363" href="{{ URL::route('stones-search-param', 'Beine') }}" alt="Beine">
 			
 		</map>
 @stop
