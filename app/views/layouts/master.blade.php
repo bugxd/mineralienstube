0070a0bba60b7c91e102a3049bdd6f9c4f43c512
Lukas Feistritzer <luki.feise@gmail.com>
2016-02-25 16:17:02 +0100

    .

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -16,6 +16,8 @@
 		
 		@yield('css')
 		
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 		
     </head>
     <body>
@@ -140,8 +142,6 @@
 		</div>
 		
 
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 		
 
     	@yield('scripts')
